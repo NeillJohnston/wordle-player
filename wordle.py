dbc06234af23001f88176c32b32c775226d66f86
Neill Johnston <neilljohnston30@gmail.com>
2022-02-09 00:08:36 -0500

    Lots of general improvements, can now play not in hard mode

--- wordle.py
+++ wordle.py
@@ -1,17 +1,17 @@
 from enum import Enum
 from math import log2
 from random import shuffle
+from functools import lru_cache
 import pickle
 import argparse
 
 class Outcome(Enum):
-    NONE = 0,
-    PARTIAL = 1,
-    FULL = 2
+    NULL = 0,
+    NONE = 1,
+    PARTIAL = 2,
+    FULL = 3,
 
 
-k = 0
-words = []
 start_words = {}
 abc = lambda: map(chr, range(ord('A'), ord('Z')+1))
 
@@ -19,32 +19,44 @@ DEFAULT_WORDLIST = 'data/words_alpha.txt'
 START_WORDS = 'data/start_words.pickle'
 
 
-def outcome(w, cis):
-    """Get the outcome of playing a list of guesses against a word w.
+@lru_cache(maxsize=2**15)
+def outcome(w, t):
+    """Get the outcome of playing a word w against target t. If a letter in w
+    is ., it will be disregarded.
+    """
+
+    n = len(w)
+    ft = {c: 0 for c in abc()}
+    for c in t:
+        ft[c] += 1
+
+    r = [Outcome.NULL]*n
+
+    for i in range(n):
+        if w[i] == '.':
+            continue
+
+        if w[i] == t[i]:
+            r[i] = Outcome.FULL
+            ft[w[i]] -= 1
+        else:
+            r[i] = Outcome.NONE
+
+    for i in range(n):
+        if r[i] == Outcome.NONE and ft[w[i]] > 0:
+            r[i] = Outcome.PARTIAL
+            ft[w[i]] -= 1
+
+    return tuple(r)
     
-    Each guess is of the form (c, i): a character and an index.
-    """
-    def evaluate_guess(c, i):
-        if w[i] == c:
-            return Outcome.FULL
-        k = len([1 for (c_, _) in cis[:i+1] if c == c_])
-        if c in w and k <= w.count(c):
-            return Outcome.PARTIAL
-        return Outcome.NONE
 
-    return tuple([evaluate_guess(c, i) for (c, i) in cis])
-
-
-def entropy(W, cis):
-    """Calcuate the entropy of a list of guesses played on words W.
-
-    Each guess is of the form (c, i): a character and an index.
-    """
-    n = len(W)
+def entropy(w, T):
+    """Calcuate the entropy of playing a word against a word set T."""
+    n = len(T)
     X = {}
 
-    for w in W:
-        r = outcome(w, cis)
+    for t in T:
+        r = outcome(w, t)
         if r not in X:
             X[r] = 0
         X[r] += 1/n
@@ -53,34 +65,32 @@ def entropy(W, cis):
     return h
 
 
-def mask_to_cis(mask):
-    return [(c, i) for (i, c) in enumerate(mask) if c != '_']
-
-
-def make_guess(W, a=1):
-    """Make a guess based on the available words W.
-
-    Set hard_mode to True to only allow guesses from the given word set, otherwise
-    guesses from all words.
+def make_guess(W, T, k, a=3):
+    """Make a guess based on the available target wordset T, given available
+    choices W. T is assumed to be a subset of W.
 
     This code is probably incomprehensible unless you read the README.
     """
-    if len(W) == 0:
+    if len(T) == 0:
         return None
-    if len(W) == 1:
-        return W[0]
+    if len(T) == 1:
+        return T[0]
+
+    def single_mask(c, i):
+        m = ['.'] * k
+        m[i] = c
+        return ''.join(m)
 
     # H(Y_ci)
     HY = {}
     for c in abc():
-        for i in range(len(W[0])):
-            HY[(c, i)] = entropy(W, [(c, i)])
+        for i in range(k):
+            HY[(c, i)] = entropy(single_mask(c, i), T)
 
     # Estimated H(X_w)
     EHX = []
     for w in W:
-        cis = mask_to_cis(w)
-        h = sum(HY[(c, i)] for (c, i) in cis)
+        h = sum(HY[(c, i)] for (i, c) in enumerate(w))
         EHX.append((h, w))
 
     EHX.sort(reverse=True)
@@ -88,42 +98,50 @@ def make_guess(W, a=1):
     if n == 0:
         return None
     # Check a% of the words, and at least 10
-    HX = EHX[:min(n, int(10 + a*n/100))]
-    HX = [(entropy(W, mask_to_cis(word)), word) for (_, word) in HX]
+    HX = EHX[:min(n, int(10 + a*n/10))]
+    HX = [(entropy(w, T), w) for (_, w) in HX]
 
-    return max(HX)[1]
+    (hw, w) = max(HX)
+    if w in T:
+        return w
+    # Entropy remaining
+    hr = log2(len(T))
+    (ht, t) = max([(entropy(t, T), t) for t in T])
+    # Expected turns remaining if we play a non-target word    = 2^(hr - hw)/2
+    # Expected turns remaining if we play the best target word = (|T|-1)/|T| * 2^(hr - ht)/2
+    if 2**(hr - hw)/2 > (len(T)-1)/len(T) * 2**(hr - ht)/2:
+        return t
+    return w
 
 
-def play(target):
+def play(t, W, T, k):
     """Play out a game with a given target word, and return the list of turns
     made.
     """
     start = True
-    W = words
     turns = []
-    while len(W) >= 1:
+    while len(T) >= 1:
         if start and k in start_words:
             w = start_words[k]
         else:
-            w = make_guess(W)
+            w = make_guess(W, T, k)
         start = False
         if w is None:
             print('could not guess word')
             return None
 
-        cis = mask_to_cis(w)
-        r = outcome(target, cis)
-        W = [t for t in W if r == outcome(t, cis)]
+        r = outcome(w, t)
+        T = [t_ for t_ in T if r == outcome(w, t_)]
 
         turns.append(w)
-        if w == target:
+        if w == t:
             break
 
     return turns
 
 
-def play_interactive():
-    """Play an interactive session against an unknown target word.
+def play_interactive(W, T, k):
+    """Play an interactive session against an unknown target word of length k.
 
     Each turn, a guess will be given. The user needs to type in the outcome of
     the guess, using g/y/. to represent full/partial/none (green/yellow/gray)
@@ -146,17 +164,16 @@ def play_interactive():
         return (None, tuple(r))
 
     start = True
-    W = words
     while True:
         if start and k in start_words:
             w = start_words[k]
         else:
-            w = make_guess(W)
+            w = make_guess(W, T, k)
         start = False
         if w is None:
             print('! Could not guess word, it\'s probably not in the wordlist')
             break
-        print(f'> {w} ({len(W)} words)')
+        print(f'> {w} ({len(T)} words)')
 
         r = None
         while True:
@@ -170,7 +187,7 @@ def play_interactive():
                 print('!', err)
                 continue
             else:
-                W = [t for t in W if outcome(t, mask_to_cis(w)) == r]
+                T = [t for t in T if outcome(w, t) == r]
                 break
 
         if r is None:
@@ -179,24 +196,26 @@ def play_interactive():
             break
 
 
-# TARES -> .y.y.
-# ALANE -> .gg.g
-# ...
-# FLAME
-
-def run_test(n=2000):
-    order = words
-    shuffle(order)
+def run_test(W, T, k, n=100):
+    shuffle(T)
 
     data = []
-    for word in order[:n]:
-        x = len(play(word))
+    for t in T[:n]:
+        x = len(play(t, W, T, k))
         data.append(x)
 
     print(f'k={k}')
     print(f'mean turns to win: {sum(data)/n:.2f} (n={n})')
-    print(f'prop. games lost (>6 turns): {len([1 for x in data if x > 6])/n:.2f}')
-    print(f'longest game: {max(data)}')
+    # Janky histogram incoming
+    hist = {x: 0 for x in range(0, 7)}
+    for x in data:
+        hist[x if x <= 6 else 0] += 1
+    hist_len = 40
+    top = max(hist.values())
+    bar = lambda x: '='*int(x*hist_len/top) + ' '*(hist_len - int(x*hist_len/top)) + f' | {x}'
+    for x in range(1, 7):
+        print(f'{x} | {bar(hist[x])}')
+    print(f'+ | {bar(hist[0])}')
 
 
 def main():
@@ -226,10 +245,15 @@ def main():
         help='Path to the list of valid words to use'
     )
     parser.add_argument(
+        '--targetlist',
+        default=DEFAULT_WORDLIST,
+        help='Path to the list of valid target words to use'
+    )
+    parser.add_argument(
         '--run-test',
-        action='store_true',
+        type=int,
         default=False,
-        help='Run a test on the algorithm'
+        help='Run a test on the algorithm with n iterations'
     )
     args = parser.parse_args()
     k = args.k
@@ -239,19 +263,24 @@ def main():
         words = filter(lambda w: len(w) == k, words)
         words = list(words)
 
+    with open(args.wordlist) as targets_file:
+        targets = map(lambda line: line.strip().upper(), targets_file.readlines())
+        targets = filter(lambda w: len(w) == k, targets)
+        targets = list(targets)
+
     if not args.n:
         with open(START_WORDS, 'rb') as start_words_file:
             start_words = pickle.load(start_words_file)
 
     if args.run_test:
-        run_test()
+        run_test(words, targets, k, args.run_test)
         return
 
     if args.word is not None:
-        turns = play(args.word)
+        turns = play(args.word, words, targets, k)
         print(turns)
     else:
-        play_interactive()
+        play_interactive(words, targets, k)
 
 
 if __name__ == '__main__':
